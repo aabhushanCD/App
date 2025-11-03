0781bd491027ab10f75fd98c7eb0ff279aae3cc7
aabhushanCD <dammardhakal426@gmail.com>
2025-11-03 16:59:14 +0545

    for production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/sonner";
-import { AuthContextProvider } from "./store/authStore.jsx";
+import { AuthContextProvider } from "./store/AuthStore";
 import { NotificationProvider } from "./store/NotificationStore";
 
 createRoot(document.getElementById("root")).render(
